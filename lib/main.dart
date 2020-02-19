77d5f834cb1df3fcac65111e94f594d46ab40511
rdravidpatel <30705702+rdravidpatel@users.noreply.github.com>
2020-02-19 00:07:32 -0500

    Add files via upload
    made Firebase connection with database

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+
+import 'package:f_groceries/wrapper.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        theme: new ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+          // counter didn't reset back to zero; the application is not restarted.
+
+            primaryColor: Colors.white,
+            primaryColorDark: Colors.white30,
+            accentColor: Colors.blue
+
+        ),
+      home: Wrapper(),
+    );
+  }
+}
+
---
+++ lib/services/auth.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+
+  //Sign in with email and password
+
+  //Register with email and password
+
+  //Sign out
+
+}
