2387086ffa859e61a43ae76f86f577eb2bc68b3f
Abhishek3098 <aspandya30398@gmail.com>
2020-03-05 01:12:38 -0500

    Made Signout Method
    User needs to sign-off button to sign-out from the account

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-
+import 'package:f_groceries/model/user.dart';
+import 'package:f_groceries/services/auth.dart';
 import 'package:f_groceries/wrapper.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:provider/provider.dart';
 
 void main() => runApp(new MyApp());
 
@@ -10,24 +10,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: new MaterialApp(
         theme: new ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-          // counter didn't reset back to zero; the application is not restarted.
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+            // counter didn't reset back to zero; the application is not restarted.
 
             primaryColor: Colors.white,
             primaryColorDark: Colors.white30,
-            accentColor: Colors.blue
-
-        ),
-      home: Wrapper(),
+            accentColor: Colors.blue),
+        home: Wrapper(),
+      ),
     );
   }
 }
-
--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,12 +1,21 @@
 import 'package:f_groceries/authenticate.dart';
 import 'package:f_groceries/home.dart';
+import 'package:f_groceries/model/user.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final user = Provider.of<User>(context);
+    print(user);
+
     // return either Home and or Authenticate widget
-    return Authenticate();
+    if(user == null){
+      return Authenticate();
+    } else{
+      return Home();
+    }
   }
 }
